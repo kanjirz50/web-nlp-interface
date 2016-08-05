fe7085bc929d3af4c4141b112493bbb01259abe6
Kanji Takahashi <takahashi@jnlp.org>
2016-08-05 13:17:10 +0900

    add comments

--- app/controllers/sample.py
+++ app/controllers/sample.py
@@ -7,14 +7,22 @@ import your_tool
 
 @route('/')
 def index():
+    """
+    Welcome page
+    """
     return template("sample")
 
 @get('/api/analyzer')
 def return_analysis_result():
-    text = request.query.input_text# .encode("utf-8")
-
+    """
+    Example of analyzer API(GET)
+    """
+    # Get submitted contents.
+    text = request.query.input_text
+    # Analyze.
     _text = your_tool.lowercase(text)
 
+    # Make a request.
     body = json.dumps(_text)
     r = HTTPResponse(status=200, body=body)
     r.set_header('Content-Type', 'application/json')
--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libs'))
 
 from bottle import route, static_file, default_app
+# load controller files
 from app.controllers import *
 
 
@@ -14,9 +15,16 @@ from app.controllers import *
 def static(filepath):
     return static_file(filepath, root='./static/')
 
-## bottle単品で動かしたい場合
-from bottle import run
-run(host='localhost', port=8080, debug=True, reloader=True)
+## Run with bottle http server.
+# from bottle import run
+# run(host='localhost', port=8080, debug=True, reloader=True)
+# e.g. python index.py
 
-## gunicornを使う場合
-#app = default_app()
+## Run with gunicorn
+app = default_app()
+# e.g. gunicorn -b 127.0.0.1:8080 -w 1 index:app
+
+## Run with PasteServer
+# from bottle import run, PasteServer
+# run(server=PasteServer, host='localhost', port=8080, debug=True, reloader=True)
+# e.g. python index.py
